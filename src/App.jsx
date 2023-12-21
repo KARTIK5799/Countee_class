117287a29dbc19e0e8f0bd86b1a23b108b8c5a0d
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-21 08:33:10 +0530

    class based counter

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './components/Counter/Counter';
+
+function App() {
+  return (
+    <div className="bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 min-h-screen flex items-center justify-center">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
